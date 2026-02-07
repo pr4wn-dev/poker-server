ba47559311a1cd37a4aca16ab3e2c7345e0a540f
bobby yontz <megapr4wn@gmail.com>
2026-02-06 19:45:04 -0500

    Fix PowerShell syntax - remove nested if, use separate if statements

--- monitoring/monitor.ps1
+++ monitoring/monitor.ps1
@@ -1875,11 +1875,11 @@ while ($monitoringActive) {
             if ($serverHasSimulation -and $unityIsInGame) {
                 # Both server and Unity agree: simulation is active
                 $stats.SimulationRunning = $true
-            } elseif ($serverHasSimulation) {
-                if ($unityIsInGame -eq $false) {
-                    # Server has simulation but Unity isn't connected to it - treat as inactive
-                    # This means there's an orphaned simulation (bots playing without Unity)
-                    $stats.SimulationRunning = $false
+            }
+            if ($serverHasSimulation -and ($unityIsInGame -eq $false)) {
+                # Server has simulation but Unity isn't connected to it - treat as inactive
+                # This means there's an orphaned simulation (bots playing without Unity)
+                $stats.SimulationRunning = $false
                 
                 # Track last time we tried to stop orphaned simulations (prevent spam)
                 if (-not $script:lastOrphanedSimStopAttempt) {
