08e53c4abfa1be19bfd5fb729d31d65abbe3b992
bobby yontz <megapr4wn@gmail.com>
2026-02-06 19:41:25 -0500

    Fix PowerShell syntax - move variable declaration inside elseif block

--- monitoring/monitor.ps1
+++ monitoring/monitor.ps1
@@ -1872,13 +1872,12 @@ while ($monitoringActive) {
             $serverHasSimulation = $logWatcherStatus.ActiveSimulations -gt 0
             $unityActualStatus = Get-UnityActualStatus
             $unityIsInGame = $unityActualStatus.InGameScene -and $unityActualStatus.ReceivingGameUpdates
-            $unityNotInGame = -not $unityIsInGame
-            
             if ($serverHasSimulation -and $unityIsInGame) {
                 # Both server and Unity agree: simulation is active
                 $stats.SimulationRunning = $true
-            }
-            if ($serverHasSimulation -and $unityNotInGame) {
+            } elseif ($serverHasSimulation) {
+                $unityNotInGame = -not $unityIsInGame
+                if ($unityNotInGame) {
                 # Server has simulation but Unity isn't connected to it - treat as inactive
                 # This means there's an orphaned simulation (bots playing without Unity)
                 $stats.SimulationRunning = $false
