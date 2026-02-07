148037aa48256312bf5ea04fa9e66870c48ab374
bobby yontz <megapr4wn@gmail.com>
2026-02-06 19:45:38 -0500

    Remove extra closing brace

--- monitoring/monitor.ps1
+++ monitoring/monitor.ps1
@@ -1946,7 +1946,6 @@ while ($monitoringActive) {
                         Write-ConsoleOutput -Message "[$(Get-Date -Format 'HH:mm:ss')] ⚠️  SIMULATION: Orphaned simulation detected (throttled - will stop in $([math]::Round(10 - $timeSinceLastStop.TotalSeconds))s)" -ForegroundColor "Yellow"
                     }
                 }
-                } # End if ($unityIsInGame -eq $false)
             }
             if (-not $serverHasSimulation) {
                 # No simulation on server
